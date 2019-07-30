e721fe5d5d98ac18f1f6b60012379d9ba99113a1
Guanli Liu <liuguanli1205@qq.com>
2019-07-30 16:21:10 +1000

    change ignore

--- src/com/unimelb/cis/Main.java
+++ src/com/unimelb/cis/Main.java
@@ -1,4 +1,13 @@
 package com.unimelb.cis;
 
+import com.unimelb.cis.structures.zrtree.ZRtree;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ZRtree zRtree = new ZRtree();
+        zRtree.buildRtree("datasets/normal_2000000_.csv");
+        zRtree.output("datasets/Z_normal_2000000_.csv");
+    }
+
 }
